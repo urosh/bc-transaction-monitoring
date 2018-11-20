a2251b0fce37ecaa0fa49b2eb7bf46bcc8cdfeb1
Uros Damnjanovic <uros@pumapay.io>
2018-11-20 23:06:11 +0200

    Adding config

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
-
-
-
 export class BlockhainMonitor {
-  public constructor () {
-
+  private checkInterval: any;
+  private checkIntervalValue: number;
+  public constructor (config?: Config) {
+    this.checkIntervalValue = config.checkInterval || 15000;
   }
   
   private init() {
+    clearTimeout(this.checkInterval);
+    setTimeout(this.checkUpdates, this.checkIntervalValue);
+  }
 
+  private checkUpdates() {
+    
   }
 
   private addAddress() {
 
   }
+}
+
+interface Config {
+  checkInterval?: number;
+
+
 }
